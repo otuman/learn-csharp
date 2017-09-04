5af2428423a57b183c66bfbf434e91aa24d32cd9
Nkomanya Otoman <otomankgm@rocketmail.com>
2017-09-04 14:07:42 -0400

    Added vidly project

---
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+/*                               Action Results   
+ * 
+ *           Type                                    Helper Methods
+ *           
+ *           ViewResult                             View()
+ *           PartialViewResult                      PartialView()
+ *           ContentResult                          Content()
+ *           RedirectResult                         Redirect()
+ *           RedirectToRouteResult                  RedirectToAction()
+ *           JsonResult                             Json()
+ *           FileResult                             File()
+ *           HttpNotFoundResult                     HttpNotFound()
+ *           EmptyResult
+ * 
+ */
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: movies/random
+        public ActionResult random(){
+            var movie = new Movie() { Name = "Otoman" };
+
+           // return View(movie);
+           // return Content("Hello world");
+            return HttpNotFound();
+
+        }
+    } 
+}
