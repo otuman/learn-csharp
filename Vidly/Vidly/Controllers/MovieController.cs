0c2f40dd97dab944ef9252706fa1cb79e4e98ab9
Nkomanya Otoman <otomankgm@rocketmail.com>
2017-09-04 14:42:42 -0400

    Added vidly project

--- Vidly/Vidly/Controllers/MovieController.cs
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -25,14 +25,59 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        // More about Action Results   
+
         // GET: movies/random
-        public ActionResult random(){
+        public ActionResult random()
+        {
             var movie = new Movie() { Name = "Otoman" };
 
-           // return View(movie);
-           // return Content("Hello world");
-            return HttpNotFound();
+            // return View(movie);
+            // return Content("Hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+
+            return RedirectToAction("index", "home", new { page = "users", orderby = "date" });
 
         }
-    } 
+
+
+        /* More about Action Parameter  
+         *    Parameter sources(Parametr binding)
+         *    
+         *   In the URL : movies/edit/1
+         *   
+         *   In the query string: /movies/edit/?id=1
+         *   
+         *   In the form data: id=1
+         * 
+         */
+        public ActionResult Edit(int id)
+        {
+
+            return Content("id=" + id);
+        }
+
+
+        /* More about optional Parameter  
+         *    Parameter sources(Parametr binding)
+         *    
+         *   In the URL : movies/edit/1
+         *   
+         *   In the query string: /movies/edit/?id=1
+         *   
+         *   In the form data: id=1
+         * 
+         */
+        public ActionResult Index(int? pageIndex, string sortby)
+        {
+
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortby))
+                sortby = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortby={1}", pageIndex, sortby));
+        }
+    }
 }
